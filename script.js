fd6c0435541510112dfd53a8222ca4f88c652a8d
Krushna-T1 <152420714+Krushna-T1@users.noreply.github.com>
2024-06-04 10:43:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+let keys=["1","2","3","4","5","6","7","8","9","0","+","-","*","/"];
+document.addEventListener("keyup",function getInput(e){
+    console.log(e.key);
+        if(e.key=="Backspace"){
+            back();
+            console.log(e.key);
+        }
+        else if(e.key=="Enter" ||e.key=="=")
+        {
+            cal();
+            console.log(e.key);
+        }else if(e.key=="%"){
+            per();
+        }
+       keys.forEach((k)=>{
+        if(e.key==k){
+            get(k);
+            console.log(k);
+        }
 
+       })
+})
 function get(val){
     document.getElementById("input").value +=val;
+    
    
     document.getElementById("output").value =eval(document.getElementById("input").value);
    
@@ -24,6 +46,11 @@ function per(){
 }
 function back(){
     document.getElementById("input").value =document.getElementById("input").value.slice(0,-1);
+}
+
+function botton(event){
+    let key = event.key;
+    document.getElementById("input").innerHTML += key;
 }
 
 
